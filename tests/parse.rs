4e86124a96ce7d2f998d63805b9c8c59affa9447
Miles Johnson <milesj@users.noreply.github.com>
2025-06-25 01:06:05 -0700

    feat: Support godebug and tool directives and Windows carriage return (#15)

--- tests/parse.rs
+++ tests/parse.rs
@@ -1,4 +1,5 @@
 use gomod_parser::GoMod;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::path::PathBuf;
 use std::str::FromStr;
@@ -59,4 +60,44 @@ fn test_parse_on_prometheus() {
     let gomod = file_content.parse::<GoMod>().unwrap();
 
     assert_eq!(gomod.module, "github.com/prometheus/prometheus".to_string());
+}
+
+#[test]
+fn test_parse_godebug() {
+    let file_content = get_test_file_content("godebug.mod");
+    let gomod = file_content.parse::<GoMod>().unwrap();
+
+    assert_eq!(
+        gomod.godebug,
+        HashMap::from_iter([
+            ("asynctimerchan".into(), "0".into()),
+            ("default".into(), "go1.21".into()),
+            ("panicnil".into(), "1".into()),
+        ])
+    );
+}
+
+#[test]
+fn test_parse_tool() {
+    let file_content = get_test_file_content("tool.mod");
+    let gomod = file_content.parse::<GoMod>().unwrap();
+
+    assert_eq!(
+        gomod.tool,
+        vec![
+            "example.com/mymodule/cmd/mytool1",
+            "example.com/mymodule/cmd/mytool2"
+        ]
+    );
+}
+
+#[test]
+fn test_carriage_return() {
+    let file_content = get_test_file_content("compress.mod")
+        .replace("\r", "") // Replace any current CR
+        .replace("\n", "\r\n"); // Replace LF with CRLF
+
+    let gomod = file_content.parse::<GoMod>().unwrap();
+
+    assert_eq!(gomod.module, "github.com/klauspost/compress".to_string());
 }
